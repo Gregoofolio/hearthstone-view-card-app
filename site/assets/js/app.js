ccfa5dc45cae57b77d8948962f67302964c27523
Grzesiek <grzesiek.kyc@gmail.com>
2015-10-12 15:52:49 +0200

    added basic add-to-list functionality

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -34,20 +34,50 @@
 
 	.directive('tower', function(){
 		return{
-			restrict: 'E',
+			restrict: 'EA',
 			scope:{
-				graphHeight: '@getHeight'
+				numberOf0: '@getZero',
+				numberOf1: '@getOne',
+				numberOf2: '@getTwo',
+				numberOf3: '@getThree',
+				numberOf4: '@getFour',
+				numberOf5: '@getFive',
+				numberOf6: '@getSix',
+				numberOf7: '@getSeven',
+				numberOf8: '@getEight',
+				numberOf9: '@getNine',
+
 			},
 			link: function(scope, element, attrs){
-				var tower, animateUp;
+				// define vars
+				var tower,graph0, animateUp;
+				//define html elements
 				tower = angular.element(element);
+				graph0 = tower.children()[0];
+				graph1 = tower.children()[1];
+				graph2 = tower.children()[2];
+				graph3 = tower.children()[3];
+				graph4 = tower.children()[4];
+				graph5 = tower.children()[5];
+				graph6 = tower.children()[6];
+				graph7 = tower.children()[7];
+				graph8 = tower.children()[8];
+				graph9 = tower.children()[9];
 
 				animateUp = function(){
-					$(this).css("height", scope.graphHeight*10 + 'px');
-					console.log(scope.graphHeight)
+					$(graph0).css("height", scope.numberOf0*15 + 'px');
+					$(graph1).css("height", scope.numberOf1*15 + 'px');
+					$(graph2).css("height", scope.numberOf2*15 + 'px');
+					$(graph3).css("height", scope.numberOf3*15 + 'px');
+					$(graph4).css("height", scope.numberOf4*15 + 'px');
+					$(graph5).css("height", scope.numberOf5*15 + 'px');
+					$(graph6).css("height", scope.numberOf6*15 + 'px');
+					$(graph7).css("height", scope.numberOf7*15 + 'px');
+					$(graph8).css("height", scope.numberOf8*15 + 'px');
+					$(graph9).css("height", scope.numberOf9*15 + 'px');
 				}
 
-				$(tower).on('click', animateUp);
+				$(tower).on('mouseover', animateUp);
 			}
 
 		}
--- site/assets/js/controllers.js
+++ site/assets/js/controllers.js
@@ -4,9 +4,7 @@
 	.controller("MainController", ["$scope","$http", function($scope, $http){
 		$scope.order = 'name';
 
-
-
-		// 1. Getting data from api (with promise)
+		// 1. Getting data from api (with promise function below)
 		var onCardsComplete = function(response){
 			$scope.cards = response.data;
 		};
@@ -17,25 +15,87 @@
 				.then(onCardsComplete);
 		// 1. END
 				
+		//2. Counting in list of added cards (arrayOfCost) how may times each cost appears
+			// vars
 
-		var arrayOfCost =[],
-			totalCardCost = 0 ;
-		$scope.totalCardCost = totalCardCost;
-		$scope.arrayOfCost = arrayOfCost;
+		$scope.totalCardCost = 0;
+		$scope.arrayOfCost = [];
+		$scope.arrayOfNames = [];
+		$scope.arrayOfAttack = [];
+		$scope.arrayOfHealth = [];
+		$scope.numberOf0 = 0;
+		$scope.numberOf1 = 0;
+		$scope.numberOf2 = 0;
+		$scope.numberOf3 = 0;
+		$scope.numberOf4 = 0;
+		$scope.numberOf5 = 0;
+		$scope.numberOf6 = 0;
+		$scope.numberOf7 = 0;
+		$scope.numberOf8 = 0;
+		$scope.numberOf9 = 0;
+
+			// function counting how many times each cost apeears in arrayOfCosts
+			function countInArray(array, what){
+				var count = 0;
+				for(var i =0; i<array.length; i++){
+					if (array[i] === what){
+						count++
+					}
+				}
+				return count;
+			}
+		//on plus-button click get card from ng-repeat and send its data via function addToList.
 		$scope.addToList = function(card) {
 
 			$scope.cost = card.cost;
+			$scope.attack = card.attack;
+			$scope.health = card.health;
+			$scope.cardName = card.name;
+
+			//adding costs to array and total value. Then invoke function countInArray for each cost
 			if (isNaN($scope.cost)){
-				alert("This card doesnt have cost, its probably Hero")
-			} else{
+				alert("This card "+ "("+ $scope.cardName +")" + " does not have cost, its probably Hero");
+				$scope.arrayOfCost.push('-');
+			} else {
 				$scope.totalCardCost += $scope.cost;
 				$scope.arrayOfCost.push($scope.cost);
 				console.log("Total cost: " + $scope.totalCardCost);
 				console.log("Array of costs: "+ $scope.arrayOfCost);
 			}
+			$scope.numberOf0 = countInArray($scope.arrayOfCost, 0);
+			$scope.numberOf1 = countInArray($scope.arrayOfCost, 1);
+			$scope.numberOf2 = countInArray($scope.arrayOfCost, 2);
+			$scope.numberOf3 = countInArray($scope.arrayOfCost, 3);
+			$scope.numberOf4 = countInArray($scope.arrayOfCost, 4);
+			$scope.numberOf5 = countInArray($scope.arrayOfCost, 5);
+			$scope.numberOf6 = countInArray($scope.arrayOfCost, 6);
+			$scope.numberOf7 = countInArray($scope.arrayOfCost, 7);
+			$scope.numberOf8 = countInArray($scope.arrayOfCost, 8);
+			$scope.numberOf9 = countInArray($scope.arrayOfCost, 9);
+
+			//adding each card attack to array of attacks
+			if($scope.attack === undefined){
+				($scope.arrayOfAttack).push('-')
+				} else {
+					($scope.arrayOfAttack).push($scope.attack);
+			};
+			//adding each card health to array of healths
+			if($scope.health === undefined){
+				($scope.arrayOfHealth).push('-')
+				} else {
+					($scope.arrayOfHealth).push($scope.health);
+			};
+			//adding each card name to array of names
+			$scope.arrayOfNames.push($scope.cardName);
+
+			console.log("Array of attack: "+ $scope.arrayOfAttack);
+			console.log("Array of health: "+ $scope.arrayOfHealth);
+			console.log("Array of names: "+ $scope.arrayOfNames);
 		}
-		
+		// In this stage I have Arrays of names,health,attack and cost each card which was clicked. 
+		//2. END
 	}])
+
 
 	.controller("DetailController", ["$scope","$routeParams","$http", function ($scope, $routeParams, $http ){
 
@@ -52,8 +112,6 @@
 				.then(onDetailComplete);
 		
 	}]);
-	
-
 	
 	
 // Controllers END
